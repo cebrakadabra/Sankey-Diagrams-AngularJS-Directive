a524e65d2574354052a62093cd66c15ddfc2a0d8
Christoph Eberl <office@cebra-webdesign.at>
2015-01-22 16:15:24 +0100

    watch animation test ready

--- js/view.js
+++ js/view.js
@@ -1,10 +1,10 @@
-d3app.directive('d3sankeyDirective', function() {
+d3app.directive('d3sankeyDirective', function($parse) {
     return {
       restrict: 'E',
       scope: {
 	      config: '='
 	    },
-      	link: function postLink(scope, element, attrs, $watch, $timeout) {
+      	link: function postLink(scope, element, attrs, $watch) {
 
       		var nodes = [];
       		var links = []; 
@@ -45,6 +45,14 @@ d3app.directive('d3sankeyDirective', function() {
       			nodes: [],
       			links: []
       		};
+
+      		for(var i = 0; i < nodes.length; i++){
+	  			structure.nodes.push({"name": nodes[i].name});
+	  		}
+
+	  		for(var y = 0; y < links.length; y++){
+	  			structure.links.push({"source": links[y].source, "target": links[y].target, "value": links[y].value});
+	  		}
 
 
 
@@ -146,25 +154,21 @@ d3app.directive('d3sankeyDirective', function() {
 	
 
 
-	scope.init = function(wval, hval){
+	//scope.init = function(wval, hval){
 			var w = null,
 			    h = null;
 
 			if(scope.config.width == null && scope.config.height == null && scope.config.autosize == true){
 				// w and h = null, autosize true
-				console.log("w and h = null, autosize true");
 				if($("#"+clusteridentifier+"").parent().height() == 0 || $("#"+clusteridentifier+"").parent().width() == 0){
 					if($("#"+clusteridentifier+"").parent().parent().height() == 0 || $("#"+clusteridentifier+"").parent().parent().width() == 0){
 						w = $("body").width();
 						h = $("body").height();
-			    		console.log("body?");
 						scope.drawChart(w, h);
 
 					} else{
-						console.log("here?");
 						w = $("#"+clusteridentifier+"").parent().parent().width();
 						h = $("#"+clusteridentifier+"").parent().parent().height();
-					    console.log(w, h);
 						scope.drawChart(w, h);
 						
 					}
@@ -179,19 +183,15 @@ d3app.directive('d3sankeyDirective', function() {
 
 			} else if(scope.config.width != null && scope.config.height != null && scope.config.autosize == true){
 				// w and h != null, but autosize true
-				console.log("w and h != null, but autosize true");
 				if($("#"+clusteridentifier+"").parent().height() == 0 || $("#"+clusteridentifier+"").parent().width() == 0){
 					if($("#"+clusteridentifier+"").parent().parent().height() == 0 || $("#"+clusteridentifier+"").parent().parent().width() == 0){
 						w = $("body").width();
 						h = $("body").height();
-			    		console.log("body?");
 						scope.drawChart(w, h);
 
 					} else{
-						console.log("here?");
 						w = $("#"+clusteridentifier+"").parent().parent().width();
 						h = $("#"+clusteridentifier+"").parent().parent().height();
-					    console.log(w, h);
 						scope.drawChart(w, h);
 						
 					}
@@ -204,20 +204,15 @@ d3app.directive('d3sankeyDirective', function() {
 
 			} else{
 				// w and h in config set
-				w = wval;
-				h = hval;
+				w = scope.config.width;
+				h = scope.config.height;
 
 				scope.drawChart(w, h);
-				// var margin = {top: 1, right: 1, bottom: 6, left: 1};
-				// var svg = d3.select("#"+sankeyidentifier+">svg")
-				//     .attr("width", 100 + margin.left + margin.right)
-				//     .attr("height", 100 + margin.top + margin.bottom)
-				//   .append("g")
-				//     .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+				
 				
 			}
 
-		};
+//		};
 			
 
 
@@ -227,30 +222,85 @@ d3app.directive('d3sankeyDirective', function() {
 	
 	scope.$watch('config', function(newconf, oldconf) {
 
-	  	console.log(oldconf);
-	  	console.log(newconf);
-	  	$("#"+sankeyidentifier+" > svg").fadeOut();
-	  	$("#"+sankeyidentifier+" > svg").remove();
-	 	
+	  	var margin = {top: 1, right: 1, bottom: 6, left: 1},
+			    width = newconf.width - margin.left - margin.right,
+			    height = newconf.height - margin.top - margin.bottom;
 
-		
-		structure.nodes = [];
-		structure.links = [];
-	  	nodes = [];
-	  	links = [];
+	  	var formatNumber = d3.format(",.0f"),
+				    format = function(d) { return formatNumber(d) + " TWh"; },
+				    //color = d3.scale.category20();
+				    color = d3.scale.ordinal()
+          					.range(groupColors)
+          					.domain(d3.range(0,8));
 
-	  	nodes = newconf.nodes;
-	  	links = newconf.links;
+	  	var svg = d3.select("#"+sankeyidentifier+" svg");
 
-	  	for(var i = 0; i < nodes.length; i++){
-  			structure.nodes.push({"name": nodes[i].name});
-  		}
+		var sankey = d3.sankey(width)
+				    .nodeWidth(15)
+				    .nodePadding(10)
+				    .size([width, height]);
 
-  		for(var y = 0; y < links.length; y++){
-  			structure.links.push({"source": links[y].source, "target": links[y].target, "value": links[y].value});
-  		}
+		var path = sankey.link();
 
-	  	scope.init(newconf.width, newconf.height);
+
+		var energy = newconf;
+		//d3.json("json/energy.json", function(energy) {
+
+		  sankey
+		      .nodes(energy.nodes)
+		      .links(energy.links)
+		      .layout(32);
+
+			var link = svg.selectAll("path.link")
+		      .data(energy.links)
+		      .attr("d", path)
+		      .style("stroke-width", function(d) { return Math.max(1, d.dy); })
+		      .sort(function(a, b) { return b.dy - a.dy; });
+
+		  link.select("title")
+		      .text(function(d) { return d.source.name + " → " + d.target.name + "\n" + format(d.value); });
+
+		  
+
+		  var node = svg.selectAll("g.node")
+		  			  .data(energy.nodes)
+		  			  .call(d3.behavior.drag()
+				      .origin(function(d) { return d; })
+				      .on("dragstart", function() { this.parentNode.appendChild(this); })
+				      .on("drag", dragmove))
+		  			  .transition()
+				      .ease("ease")
+				      .duration(250)
+				      .attr("class", "node")
+					  .attr("transform", function(d) { return "translate(" + d.x + "," + d.y + ")"; });
+
+		  node.select("rect")
+		      .attr("height", function(d) { return d.dy; })
+		      .attr("width", sankey.nodeWidth())
+		      .style("fill", function(d) { return d.color = color(d.name.replace(/ .*/, "")); 
+		      })
+		      .style("stroke", function(d) { return d3.rgb(d.color).darker(2); })
+		    .select("title")
+		      .text(function(d) { return d.name + "\n" + format(d.value); });
+
+		  node.select("text")
+		      .attr("x", -6)
+		      .attr("y", function(d) { return d.dy / 2; })
+		      .attr("dy", ".35em")
+		      .attr("text-anchor", "end")
+		      .attr("transform", null)
+		      .text(function(d) { return d.name; })
+		    .filter(function(d) { return d.x < width / 2; })
+		      .attr("x", 6 + sankey.nodeWidth())
+		      .attr("text-anchor", "start");
+
+		  function dragmove(d) {
+		    d3.select(this).attr("transform", "translate(" + d.x + "," + (d.y = Math.max(0, Math.min(height - d.dy, d3.event.y))) + ")");
+		    sankey.relayout();
+		    link.attr("d", path);
+		  }
+
+
 	  
 	});
 
